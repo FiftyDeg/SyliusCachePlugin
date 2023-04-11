c2686520ddcd180fcbbf4a723f12df03b72fcd6d
Filippo Maria Tasca <filippo.maria.tasca@gmail.com>
2023-04-11 11:21:30 +0200

    #863gb2kyh [SYS] Dockerized plugin.

--- .docker/bin/make_dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker-compose exec app make init && \
-docker exec -it sylius-cache-plugin_app_1 sh
-
--- .docker/bin/make_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker-compose exec app make init-test && \
-docker exec -it sylius-cache-plugin_app_1 sh
-
